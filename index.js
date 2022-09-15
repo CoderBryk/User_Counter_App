75ac2aaaef825cb2f9206346a68a7744c679a0e8
BlossomEze <107925825+BlossomEze@users.noreply.github.com>
2022-09-15 19:59:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.innerText = 0
+    count = 0
+}
+
+// Google:
+// innerText alternative mdn
